1aef0f507e29e6dfdb71fbfe8d642a1b20c1cfc6
httpjamesm <github@httpjames.space>
2024-03-03 10:50:14 -0500

    feat: icons

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,6 +1,5 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
-use tauri_plugin_store::StoreBuilder;
 
 fn main() {
     tauri::Builder::default()
