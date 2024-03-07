bea5ee9230d5ee9259c45627594b98717015a646
httpjamesm <github@httpjames.space>
2024-03-06 21:53:57 -0500

    fix: use goto

--- src/lib/components/ArtistList.svelte
+++ src/lib/components/ArtistList.svelte
@@ -1,11 +1,17 @@
 <script lang="ts">
 	import type { ArtistItem } from '$lib/interfaces/artist';
+	import { goto } from '$app/navigation';
+
 	export let artists: ArtistItem[];
 </script>
 
 {#each artists as artist, index (artist.id)}
-	<a class="item-reference-link" href="/app/artist?id={artist.id}" on:click|stopPropagation
-		>{artist.name}</a
+	<a
+		href={null}
+		class="item-reference-link"
+		on:click|stopPropagation={() => {
+			goto(`/app/artist?id=${artist.id}`);
+		}}>{artist.name}</a
 	>{#if index !== artists.length - 1}
 		<span>, </span>
 	{/if}
