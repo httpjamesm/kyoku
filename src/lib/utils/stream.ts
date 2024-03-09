2d923aa0b7e2e28e277454e9fd50b9df75857d1b
httpjamesm <github@httpjames.space>
2024-03-09 01:00:53 -0500

    fix: add ogg support

--- src/lib/utils/stream.ts
+++ src/lib/utils/stream.ts
@@ -2,7 +2,7 @@ import { getUrl } from '$lib/api/url';
 import { getSetting } from './settings';
 
 export const getStreamURLFromItemId = (id: string) => {
-	let url = `${getUrl(false)}/Audio/${id}/universal?audioCodec=${getSetting('playback.audioCodec')}&api_key=${window.localStorage.getItem('accessToken')}&Container=mp3,aac,m4a|aac,m4b|aac,flac,alac,m4a|alac,m4b|alac,wav&StartTimeTicks=0`;
+	let url = `${getUrl(false)}/Audio/${id}/universal?audioCodec=${getSetting('playback.audioCodec')}&api_key=${window.localStorage.getItem('accessToken')}&Container=mp3,aac,m4a|aac,ogg,m4b|aac,flac,alac,m4a|alac,ogg,m4b|alac,wav,ogg&StartTimeTicks=0`;
 
 	const quality = getSetting('playback.audioQuality');
 	if (quality !== 'auto') {
